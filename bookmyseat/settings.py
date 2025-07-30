d7ccf360cb303b93e2bcd9a85cd0cc46e1d2f0c6
avgeeeek <nxvyaa@gmail.com>
2025-07-30 21:08:29 +0530

    Add secure env and gitignore

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -59,8 +59,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'avgeek32@gmail.com'
-EMAIL_HOST_PASSWORD = 'fidf vnrn esuh gees'
+EMAIL_HOST_PASSWORD = 'fidfvnrnesuhgees'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
