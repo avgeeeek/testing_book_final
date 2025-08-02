f13c1ddd4a64717bb428881b19ce27c809f50a1e
avgeeeek <nxvyaa@gmail.com>
2025-08-02 16:48:02 +0530

    meow

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTH_USER_MODEL = 'auth.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
